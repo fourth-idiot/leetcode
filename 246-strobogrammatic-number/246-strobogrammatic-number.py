36de03b6bbd851436873a0d3d1ec32ea71601b8e
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-03-08 01:43:53 -0500

    Time: 61 ms (8.21%), Space: 14 MB (44.14%) - LeetHub

--- 246-strobogrammatic-number/246-strobogrammatic-number.py
+++ 246-strobogrammatic-number/246-strobogrammatic-number.py
@@ -1,20 +1,43 @@
 class Solution:
     def isStrobogrammatic(self, num: str) -> bool:
-        d = {
-            "0": "0",
-            "1": "1",
-            "6": "9",
-            "8": "8",
-            "9": "6"
-        }
-        i, j = 0, len(num)-1
-        while(i <= j):
-            if(num[i] not in d):
-                return False
-            elif(d[num[i]] != num[j]):
+        # d = {
+        #     "0": "0",
+        #     "1": "1",
+        #     "6": "9",
+        #     "8": "8",
+        #     "9": "6"
+        # }
+        # i, j = 0, len(num)-1
+        # while(i <= j):
+        #     if(num[i] not in d):
+        #         return False
+        #     elif(d[num[i]] != num[j]):
+        #         return False
+        #     else:
+        #         i += 1
+        #         j -= 1
+        # return True
+        
+        dp = {}
+        def isStrobogrammatic1(num: str) -> bool:
+            d = {
+                "0": "0",
+                "1": "1",
+                "6": "9",
+                "8": "8",
+                "9": "6"
+            }
+            if(num in dp):
+                return dp[num]
+            elif(num == ""):
+                dp[num] = True
+                return True
+            elif((num[0] not in d) or (d[num[0]] != num[-1])):
+                dp[num] = False
                 return False
             else:
-                i += 1
-                j -= 1
-        return True
+                subStringOutput = isStrobogrammatic1(num[1:-1])
+                dp[num[1:-1]] = subStringOutput
+                return subStringOutput
+        return isStrobogrammatic1(num)
                 
