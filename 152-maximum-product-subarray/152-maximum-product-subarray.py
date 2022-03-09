f035d84d9d320fae0b73fe6fc9af2a3f8e94fd2d
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-03-09 18:24:57 -0500

    Time: 102 ms (67.42%), Space: 14.3 MB (88.47%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.py
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -5,16 +5,7 @@ class Solution:
         maxProduct = prevMax
         prevMin = nums[0]
         for i in range(1, n):
-            currentMax = max(
-                nums[i],
-                prevMax * nums[i],
-                prevMin * nums[i]
-            )
-            currentMin = min(
-                nums[i],
-                prevMax * nums[i],
-                prevMin * nums[i]
-            )
+            currentMin, _, currentMax = sorted([nums[i], prevMin * nums[i], prevMax * nums[i]])
             maxProduct = max(maxProduct, currentMax)
             prevMax = currentMax
             prevMin = currentMin
