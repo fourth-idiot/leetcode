f148b83600069797943a87384b2f607736ede131
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-03-21 14:58:29 -0400

    Time: 70 ms (23.05%), Space: 14.3 MB (41.60%) - LeetHub

--- 274-h-index/274-h-index.py
+++ 274-h-index/274-h-index.py
@@ -4,7 +4,6 @@ class Solution:
     def hIndex(self, citations: List[int]) -> int:
         minHeap = citations.copy()
         heapq.heapify(minHeap)
-        hIndex = 0
         while((minHeap) and (len(minHeap) > minHeap[0])):
-            hIndex = heapq.heappop(minHeap)
+            heapq.heappop(minHeap)
         return len(minHeap)
