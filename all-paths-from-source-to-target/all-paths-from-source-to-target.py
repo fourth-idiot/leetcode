d2a8c783b520cebb9b0302b113fb9729d2b955d9
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-03-22 19:10:36 -0400

    Time: 212 ms, Memory: 15.9 MB - LeetHub

--- all-paths-from-source-to-target/all-paths-from-source-to-target.py
+++ all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -1,15 +1,31 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        # # Using DFS
+        # allPaths = []
+        # src, dst = 0, len(graph) - 1
+        # stack = [[src]]
+        # visited = set()
+        # while(stack):
+        #     path = stack.pop()
+        #     if(path[-1] == dst):
+        #         allPaths.append(path)
+        #     visited.add(path[-1])
+        #     for neigh in graph[path[-1]]:
+        #         stack.append(path + [neigh])
+        #     visited.remove(path[-1])
+        # return allPaths
+        
+        # Using BFS
         allPaths = []
         src, dst = 0, len(graph) - 1
-        stack = [[src, neigh] for neigh in graph[src]]
-        visited = {src}
-        while(stack):
-            path = stack.pop()
+        queue = [[src]]
+        visited = set()
+        while(queue):
+            path = queue.pop(0)
             if(path[-1] == dst):
                 allPaths.append(path)
             visited.add(path[-1])
             for neigh in graph[path[-1]]:
-                stack.append(path + [neigh])
+                queue.append(path + [neigh])
             visited.remove(path[-1])
         return allPaths
