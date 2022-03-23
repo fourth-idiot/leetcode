e27b79a659dbab743fe9f6b5e12515b816e97f2f
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-03-23 15:51:26 -0400

    Time: 71 ms (56.90%), Space: 13.9 MB (55.35%) - LeetHub

---
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -0,0 +1,30 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:                
+        numRows, numCols = len(grid), len(grid[0])
+        totalSteps = [[0 for _ in range(numCols)] for _ in range(numRows)]
+        queue = []
+        for i in range(numRows):
+            for j in range(numCols):
+                if(grid[i][j] == 2):
+                    grid[i][j] = 0
+                    queue.append((i, j))
+        directions = [(-1, 0), (0, -1), (0, 1), (1, 0)]
+        while(queue):
+            row, col = queue.pop(0)
+            for d in directions:
+                newRow = row + d[0]
+                newCol = col + d[1]
+                if((newRow >= 0) and (newRow < numRows) and
+                   (newCol >= 0) and (newCol < numCols) and
+                   (grid[newRow][newCol] == 1)):
+                    totalSteps[newRow][newCol] = totalSteps[row][col] + 1
+                    grid[newRow][newCol] = 0
+                    queue.append((newRow, newCol))
+        print(grid)
+        output = 0
+        for i in range(numRows):
+            for j in range(numCols):
+                output = max(totalSteps[i][j], output)
+                if(grid[i][j] != 0):
+                    return -1
+        return output
