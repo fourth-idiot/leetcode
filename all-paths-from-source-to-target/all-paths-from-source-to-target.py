d2e7e9f546d6857b422ed5ac7daf8188c5703939
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-03-23 13:14:02 -0400

    Time: 104 ms, Memory: 15.8 MB - LeetHub

--- all-paths-from-source-to-target/all-paths-from-source-to-target.py
+++ all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -27,5 +27,5 @@ class Solution:
             visited.add(path[-1])
             for neigh in graph[path[-1]]:
                 queue.append(path + [neigh])
-            visited.remove(path[-1])
+            # visited.remove(path[-1])
         return allPaths
