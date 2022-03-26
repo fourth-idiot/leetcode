5bcbc6b9bc2ab38804aa7edcddc6cc807426809f
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-03-26 19:57:31 -0400

    Time: 235 ms (67.20%), Space: 14.1 MB (71.20%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.py
+++ 518-coin-change-2/518-coin-change-2.py
@@ -1,11 +1,22 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        dp = [[0 for _ in range(amount + 1)] for _ in range(len(coins) + 1)]
-        for i in range(1, len(dp)):
-            dp[i][0] = 1
-        for i in range(1, len(dp)):
-            for j in range(1, len(dp[0])):
-                dp[i][j] = dp[i - 1][j]
-                if(j >= coins[i - 1]):
-                    dp[i][j] += dp[i][j - coins[i - 1]]
-        return dp[-1][-1]
+        # # Approach 1
+        # dp = [[0 for _ in range(amount + 1)] for _ in range(len(coins) + 1)]
+        # for i in range(1, len(dp)):
+        #     dp[i][0] = 1
+        # for i in range(1, len(dp)):
+        #     for j in range(1, len(dp[0])):
+        #         dp[i][j] = dp[i - 1][j]
+        #         if(j >= coins[i - 1]):
+        #             dp[i][j] += dp[i][j - coins[i - 1]]
+        # return dp[-1][-1]
+        
+        # Approach 2: Using state reduction
+        row = [0 for _ in range(amount + 1)]
+        row[0] = 1
+        for coin in coins:
+            for j in range(1, len(row)):
+                if(j >= coin):
+                    row[j] += row[j - coin]
+        return row[-1]
+        
