0a872acc8e57c9925b3acf03efb33edf88c0bf2b
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-03-28 20:47:01 -0400

    Time: 1243 ms (13.07%), Space: 40.8 MB (6.56%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -15,10 +15,27 @@ class Solution:
         #         return num
         #     seen.add(num)
         
-        # Approach 3
-        for num in nums:
-            absNum = abs(num)
-            if(nums[absNum] < 0):
-                return absNum
+        # # Approach 3
+        # for num in nums:
+        #     absNum = abs(num)
+        #     if(nums[absNum] < 0):
+        #         return absNum
+        #     else:
+        #         nums[absNum] *= -1
+        
+        # Approach 4
+        def helper(low, high):
+            nonlocal duplicate
+            if(low > high):
+                return duplicate
+            mid = (low + high) // 2
+            count = sum(num <= mid for num in nums)
+            if(count <= mid):
+                low = mid + 1
             else:
-                nums[absNum] *= -1
+                duplicate = mid
+                high = mid - 1
+            return helper(low, high)
+        low, high = 1, len(nums) - 1
+        duplicate = None
+        return helper(low, high)
