e5c997b7307523cb14a409e9edbe9406386f796f
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-04-03 01:50:47 -0400

    Time: 40 ms (95.32%), Space: 13.9 MB (79.00%) - LeetHub

--- 31-next-permutation/31-next-permutation.py
+++ 31-next-permutation/31-next-permutation.py
@@ -3,14 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        # Find the index where which will be replaced by next possible number
         if(len(nums) < 2): return nums
         p1 = len(nums) - 2
         while((p1 >= 0) and (nums[p1] >= nums[p1 + 1])):
             p1 -= 1
+        # Find the next possible number
         if(p1 >= 0):
             p2 = len(nums) - 1
             while(nums[p2] <= nums[p1]): p2 -= 1
             nums[p1], nums[p2] = nums[p2], nums[p1]
+        # Reverse the remaining list
         p1 += 1
         p2 = len(nums) - 1
         while(p1 < p2):
