daf8a1035f098193a424e93b26910cbbea6882aa
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-04-06 00:07:55 -0400

    Time: 8533 ms (5.23%), Space: 13.9 MB (100.00%) - LeetHub

---
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSumMulti(self, arr, i, target):
+        count = 0
+        freq = {}
+        for num in arr[i+1:]:
+            remaining = target - num
+            if(remaining in freq):
+                count += freq[remaining]
+            freq[num] = freq.get(num, 0) + 1
+        return count
+    
+    def threeSumMulti(self, arr: List[int], target: int) -> int:
+        arr.sort()
+        count = 0
+        for i in range(len(arr)):
+            count += self.twoSumMulti(arr, i, target - arr[i])
+        return count % (10 ** 9 + 7)
