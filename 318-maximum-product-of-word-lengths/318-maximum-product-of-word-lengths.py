800e454ad2f5d076a40f3487395f5de39f594583
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-05-29 12:57:30 -0400

    Time: 711 ms (68.36%), Space: 14.4 MB (42.09%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.py
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.py
@@ -22,7 +22,10 @@ class Solution:
 #                     maxProduct = max(maxProduct, len(w1) * len(w2))
 #         return maxProduct
 
-    # Approach 2
+    # Approach 2 (Using Bit Manipulation)
+    # A string can be represented as a 26 bit number (zyxwvutsrqponmlkjihgfedcba),
+    # where each bit represents the presence of the i'th character in the string
+    # For example, apple can be represented as 00000000001000100000010001
     def convertToBinary(self, word):
         binaryWord = 0
         for c in word:
