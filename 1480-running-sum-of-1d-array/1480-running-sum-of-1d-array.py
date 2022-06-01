d0a33a682b5fec708d98c17d40400010efaa470c
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-05-31 21:21:34 -0400

    Time: 37 ms (94.51%), Space: 14 MB (73.38%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -1,5 +1,11 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
+        # Instead of creating a new array of cumulative sums,
+        # we can replace each element with the sum of
+        # previous cumulative sum and the element itself.
+        # It will represent the cumulative sum till that element.
+        # Time complexity: O(n)
+        # Space complexity: O(1)
         for i in range(1, len(nums)):
             nums[i] += nums[i - 1]
         return nums
