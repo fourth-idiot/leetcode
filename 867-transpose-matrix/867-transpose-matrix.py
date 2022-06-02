bf868219c5bee67a9d7a91b660873d3abfb595b0
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-06-01 20:42:45 -0400

    Time: 126 ms (25.33%), Space: 14.7 MB (55.33%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.py
+++ 867-transpose-matrix/867-transpose-matrix.py
@@ -1,5 +1,11 @@
 class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        # For a matrix of dimensions m * n, initialize its transpose having shape n * m
+        # with zero values. Loop over all the elements from the original matrix
+        # and put the element from i'th row and j'th column at j'th row and i'th column
+        # in its transpose.
+        # Time Complexity: O(n^2)
+        # Space Complexity: O(n ^ 2)
         numRows, numCols = len(matrix), len(matrix[0])
         matrixT = [[0 for _ in range(numRows)] for _ in range(numCols)]
         for i in range(numRows):
