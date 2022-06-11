ef0bf4dbf10771e8c55c3c350a2c0ee5327a165b
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-06-10 21:21:14 -0500

    Time: 2264 ms (11.33%), Space: 48.4 MB (5.90%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
@@ -9,7 +9,6 @@ class Solution:
         
         leftSum = {val: index for index, val in enumerate(leftSum)}
         rightSum = {val: index for index, val in enumerate(rightSum)}
-        # print(leftSum, rightSum)
         minOp = float("inf")
         for val in leftSum:
             remainingX = x - val
@@ -19,20 +18,5 @@ class Solution:
                     minOp = min(minOp, op)
         if(minOp == float("inf")): return -1
         else: return minOp
-            
-#         print(leftSum)
-#         print(rightSum)
-# #         1, 2, 6, 8, 11
-# #         11, 10, 9, 5, 3
-        
-        
-# #         a = 0, 3, 5, 25, 26, 27, 30
-# #         b = 30, 27, 25, 5, 4, 3, 0
-        
-# #         4, 2 20, 1, 1, 3
-# #         0, 4, ...
-# #         31, 27, 25, 5, 4, 3, 0
-        
-# #         a[i] + b[j] = target such that i + j is minimum
         
         
