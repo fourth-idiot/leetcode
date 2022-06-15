cf248865c1fbf7f74fe13568bdb335abb1649e68
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-06-14 22:58:38 -0500

    Time: 1040 ms (19.95%), Space: 14 MB (99.65%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.py
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -1,23 +1,33 @@
 class Solution:
-    def isValidPair(self, str1, str2):
+    def _isValidPair(self, str1, str2):
+        # Checks if str1 is predecessor of str2.
+        # On the first character mismatch,
+        # it checks if the remaining string is equal or not.
         for i in range(len(str1)):
             if(str1[i] != str2[i]):
-                return str1[i:] == str2[(i + 1):]
+                return str1[i:] == str2[i+1:]
         return True
         
     def longestStrChain(self, words: List[str]) -> int:
+        # Sort the list of input words in the ascending order of their length
+        words.sort(key=len)
         n = len(words)
-        words.sort(key=lambda s: len(s))
+        # Initialize DP matrix with 1s as every word is a word chain of length 1
         dp = [1 for _ in range(n)]
-        for i in range(n, -1, -1):
-            maxVal = float("-inf")
+        finalMaxLen = float("-inf")
+        # For every word of length n,
+        # find length of the longest word chain starting
+        # with the word of length (n + 1)
+        for i in range(n - 1, -1, -1):
+            intermediateMaxLen = float("-inf")
             for j in range(i + 1, n):
                 if(len(words[i]) == len(words[j])):
                     continue
-                elif(len(words[i]) != len(words[j]) - 1):
+                if(len(words[i]) != len(words[j]) - 1):
                     break
-                elif(self.isValidPair(words[i], words[j])):
-                    maxVal = max(maxVal, dp[j])
-            if(maxVal != float("-inf")):
-                dp[i] += maxVal
-        return max(dp)
+                if(self._isValidPair(words[i], words[j])):
+                    intermediateMaxLen = max(intermediateMaxLen, dp[j])
+            if(intermediateMaxLen != float("-inf")):
+                dp[i] += intermediateMaxLen
+            finalMaxLen = max(finalMaxLen, dp[i])
+        return finalMaxLen
