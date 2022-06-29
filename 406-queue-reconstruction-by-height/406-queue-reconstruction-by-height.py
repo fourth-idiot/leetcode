2715a71f5ebfd4c6b54320aa9b042050f0771f33
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-06-28 19:26:03 -0500

    Time: 219 ms (24.55%), Space: 14.6 MB (31.23%) - LeetHub

---
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.py
@@ -0,0 +1,7 @@
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        people.sort(key=lambda x: (-x[0], x[1]))
+        output = []
+        for p in people:
+            output = output[:p[1]] + [p] + output[p[1]:]
+        return output
