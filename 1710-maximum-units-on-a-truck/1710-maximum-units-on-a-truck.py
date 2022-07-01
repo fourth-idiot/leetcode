66751e5b0f083df9c4cd012a284a8d05b02deed1
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-06-30 23:40:36 -0500

    Time: 3640 ms (5.01%), Space: 18.4 MB (5.52%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
@@ -1,12 +1,21 @@
 class Solution:
     def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
-        boxTypes.sort(key=lambda x: -x[1])
-        totalUnits = 0
-        remainingTruckSize = truckSize
-        for i in range(len(boxTypes)):
-            numBoxes = min(boxTypes[i][0], remainingTruckSize)
-            remainingTruckSize -= numBoxes
-            totalUnits += (numBoxes * boxTypes[i][1])
-            if(remainingTruckSize == 0):
-                break
-        return totalUnits
+        # boxTypes.sort(key=lambda x: -x[1])
+        # totalUnits = 0
+        # remainingTruckSize = truckSize
+        # for i in range(len(boxTypes)):
+        #     numBoxes = min(boxTypes[i][0], remainingTruckSize)
+        #     remainingTruckSize -= numBoxes
+        #     totalUnits += (numBoxes * boxTypes[i][1])
+        #     if(remainingTruckSize == 0):
+        #         break
+        # return totalUnits
+        
+        minHeap = []
+        heapify(minHeap)
+        for numBoxes, numUnitsPerBox in boxTypes:
+            for _ in range(numBoxes):
+                heappush(minHeap, numUnitsPerBox)
+                if(len(minHeap) > truckSize):
+                    heappop(minHeap)
+        return sum(minHeap)
