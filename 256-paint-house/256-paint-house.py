d4db106b09b9819c994dc079a9b238864992379e
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-07-01 11:58:54 -0500

    Time: 77 ms (70.21%), Space: 13.8 MB (85.04%) - LeetHub

--- 256-paint-house/256-paint-house.py
+++ 256-paint-house/256-paint-house.py
@@ -1,5 +1,13 @@
 class Solution:
     def minCost(self, costs: List[List[int]]) -> int:
+        # Approach (Using tabulation):
+        # This problem is similar to house robber problem in which we can not rob adjacent houses.
+        # Here if we color house 1 with Red color, then house 2 cannot be colored with red
+        # Hence, value cost of painting house 1 with red color can be calculated as following:
+        # dp[1][0] = cost[1][1] + min(dp[2][1] + dp[2][2])
+        
+        # In our problem, we have only 3 options to color our houses,
+        # but we will try to make code as generalized as possible.
         n, numColors = len(costs), 3
         dp = [[0 for _ in range(numColors)] for _ in range(n + 1)]
         for i in range(n - 1, -1, -1):
