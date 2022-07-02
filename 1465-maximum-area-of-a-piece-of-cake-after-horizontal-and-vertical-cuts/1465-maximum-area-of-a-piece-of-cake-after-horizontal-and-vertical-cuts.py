9f8bb18983447911fec8a7b1d904eaf4c27221af
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-07-02 11:17:30 -0500

    Time: 574 ms (20.17%), Space: 31.2 MB (9.17%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
@@ -1,5 +1,10 @@
 class Solution:
-    def getSides(self, n: int, cuts):
+    def getSides(self, n: int, cuts: List[int]) -> List[int]:
+        # Given the total length and an array of cuts on it,
+        # it returns an array of side of rectangles formed by those cuts in descending order.
+        # As the first step, we arrange all the cuts in  increasing order of their distances from one end.
+        # Then, we can find the side of rectangle formed by i'th and (i - 1)'th cut using (cuts[i] - cuts[i - 1]).
+        # Finally, we sort the array of sides in descending order as we need that rectangle with maximum area
         cuts.extend([0, n])
         cuts.sort()
         sides = [(cuts[i] - cuts[i - 1]) for i in range(1, len(cuts))]
@@ -7,6 +12,8 @@ class Solution:
         return sides
         
     def maxArea(self, h: int, w: int, horizontalCuts: List[int], verticalCuts: List[int]) -> int:
+        # Find the length and breadth of all rectangles formed by the cuts
         horizontalSides = self.getSides(h, horizontalCuts)
         verticalSides = self.getSides(w, verticalCuts)
+        # Find the area by taking the longest length and width
         return (horizontalSides[0] * verticalSides[0]) % (7 + (10 ** 9))
