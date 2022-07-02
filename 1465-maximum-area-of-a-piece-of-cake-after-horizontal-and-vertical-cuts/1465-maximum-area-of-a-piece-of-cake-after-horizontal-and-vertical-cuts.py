edc72708c64a2d039dcc2f9bb30f782fdbf49423
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-07-02 00:36:01 -0500

    Time: 492 ms (39.92%), Space: 30.9 MB (10.44%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
@@ -1,14 +1,12 @@
 class Solution:
+    def getSides(self, n: int, cuts):
+        cuts.extend([0, n])
+        cuts.sort()
+        sides = [(cuts[i] - cuts[i - 1]) for i in range(1, len(cuts))]
+        sides.sort(reverse=True)
+        return sides
+        
     def maxArea(self, h: int, w: int, horizontalCuts: List[int], verticalCuts: List[int]) -> int:
-        horizontalCuts.extend([0, h])
-        horizontalCuts.sort()
-        horizontalCutsDiff = [(horizontalCuts[i] - horizontalCuts[i - 1]) for i in range(1, len(horizontalCuts))]
-        horizontalCutsDiff.sort(reverse=True)
-        
-        verticalCuts.extend([0, w])
-        verticalCuts.sort()
-        verticalCutsDiff = [(verticalCuts[i] - verticalCuts[i - 1]) for i in range(1, len(verticalCuts))]
-        verticalCutsDiff.sort(reverse=True)
-        
-        return (horizontalCutsDiff[0] * verticalCutsDiff[0]) % (7 + (10 ** 9))
-        
+        horizontalSides = self.getSides(h, horizontalCuts)
+        verticalSides = self.getSides(w, verticalCuts)
+        return (horizontalSides[0] * verticalSides[0]) % (7 + (10 ** 9))
