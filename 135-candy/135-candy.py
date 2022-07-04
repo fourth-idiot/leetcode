3d0330858dea4b7081999a1289a1f9cd3f85c424
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-07-03 21:55:58 -0500

    Time: 385 ms (7.25%), Space: 18.5 MB (6.18%) - LeetHub

--- 135-candy/135-candy.py
+++ 135-candy/135-candy.py
@@ -1,5 +1,16 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
+        # Since each child must have at least one candy,
+        # we can initialize an unit array of length `len(ratings)`.
+        
+        # If we store all the elements and their indices in an heap,
+        # then we can GREEDILY pop the element with the lowest value.
+        # Now, we can find its neighbouring elements having higher rating.
+        # If the number of candies assigned to the child with higher ratings
+        # is less than or equal to its neighbour, then we can update its value.
+        # This way we will get an array of the final assignments,
+        # and sum of that array will be our answer.
+        
         minHeap = [(val, idx) for (idx, val) in enumerate(ratings)]
         heapify(minHeap)
         output = [1 for _ in range(len(minHeap))]
