3cfc734de6874885326c3d0e42fb88c31dac6b36
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-07-03 21:58:47 -0500

    Time: 332 ms (17.45%), Space: 18.4 MB (6.62%) - LeetHub

--- 135-candy/135-candy.py
+++ 135-candy/135-candy.py
@@ -1,8 +1,8 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
+        # Approach using Greedy and Priorirty Queue:
         # Since each child must have at least one candy,
         # we can initialize an unit array of length `len(ratings)`.
-        
         # If we store all the elements and their indices in an heap,
         # then we can GREEDILY pop the element with the lowest value.
         # Now, we can find its neighbouring elements having higher rating.
@@ -11,15 +11,18 @@ class Solution:
         # This way we will get an array of the final assignments,
         # and sum of that array will be our answer.
         
+        n = len(ratings)
+        output = [1 for _ in range(n)]
         minHeap = [(val, idx) for (idx, val) in enumerate(ratings)]
         heapify(minHeap)
-        output = [1 for _ in range(len(minHeap))]
         while(minHeap):
             val, idx = heappop(minHeap)
+            # Check if the left neighbour exists and has higher rating
             if(((idx - 1) >= 0) and
                ((ratings[idx - 1]) > ratings[idx])):
                 output[idx - 1] = max(output[idx - 1], output[idx] + 1)
-            if(((idx + 1) < len(ratings)) and
+            # Check if the right neighbour exists and has higher rating
+            if(((idx + 1) < n) and
                ((ratings[idx + 1]) > ratings[idx])):
                 output[idx + 1] = max(output[idx + 1], output[idx] + 1)
         return sum(output)
