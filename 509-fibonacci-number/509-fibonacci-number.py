d66fd848ec59d2d9da137b7f6cd804bd732fd68c
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-07-05 20:00:51 -0500

    Time: 41 ms (73.11%), Space: 13.8 MB (54.45%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -6,14 +6,23 @@ class Solution:
         # else:
         #     return self.fib(n - 1) + self.fib(n - 2)
         
-        # Approach 2 (Dynamic programming - Memoization)
-        dp = {0: 0, 1: 1}
-        def helper(n):
-            if(n in dp):
-                return dp[n]
-            if((n - 1) not in dp):
-                dp[n - 1] = helper(n - 1)
-            if((n - 2) not in dp):
-                dp[n - 2] = helper(n - 2)
-            return dp[n - 1] + dp[n - 2]
-        return helper(n)
+        # # Approach 2 (Dynamic programming - Memoization)
+        # dp = {0: 0, 1: 1}
+        # def helper(n):
+        #     if(n in dp):
+        #         return dp[n]
+        #     if((n - 1) not in dp):
+        #         dp[n - 1] = helper(n - 1)
+        #     if((n - 2) not in dp):
+        #         dp[n - 2] = helper(n - 2)
+        #     return dp[n - 1] + dp[n - 2]
+        # return helper(n)
+    
+        # Approach 3 (Dynamic programming - Tabulation)
+        if((n == 0) or (n == 1)):
+            return n
+        dp = [0 for _ in range(n + 1)]
+        dp[1] = 1
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]
