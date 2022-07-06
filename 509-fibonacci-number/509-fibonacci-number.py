2c03e289ad04a059a54862e85a0081e6f228f2a3
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-07-05 20:18:13 -0500

    Time: 38 ms (79.79%), Space: 13.8 MB (54.45%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -20,27 +20,27 @@ class Solution:
         #     return dp[n - 1] + dp[n - 2]
         # return helper(n)
     
-        # Approach 3 (Dynamic programming - Tabulation)
-        if((n == 0) or (n == 1)):
-            return n
-        dp = [0 for _ in range(n + 1)]
-        dp[1] = 1
-        for i in range(2, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
-        return dp[n]
-        
-        # # Approach 4 (Dynamic programming - Tabulation with state reduction)
-        # # From the approach 3, you can see that we need only previous 2 previous at
-        # # any stage of the algorithm. Hence, we can just store those 2 values
-        # # and get rid the dp array.
+        # # Approach 3 (Dynamic programming - Tabulation)
         # if((n == 0) or (n == 1)):
         #     return n
-        # fn_2, fn_1 = 0, 1
-        # for _ in range(2, n + 1):
-        #     # Option 1: Using an extra variable
-        #     fn = fn_1 + fn_2
-        #     fn_2 = fn_1
-        #     fn_1 = fn
-        #     # # Option 2: Python 1-liner
-        #     # fn_2, fn_1 = fn_1, fn_1 + fn_2
-        # return fn_1
+        # dp = [0 for _ in range(n + 1)]
+        # dp[1] = 1
+        # for i in range(2, n + 1):
+        #     dp[i] = dp[i - 1] + dp[i - 2]
+        # return dp[n]
+        
+        # Approach 4 (Dynamic programming - Tabulation with state reduction)
+        # From the approach 3, you can see that we need only previous 2 previous at
+        # any stage of the algorithm. Hence, we can just store those 2 values
+        # and get rid the dp array.
+        if((n == 0) or (n == 1)):
+            return n
+        fn_2, fn_1 = 0, 1
+        for _ in range(2, n + 1):
+            # Option 1: Using an extra variable
+            fn = fn_1 + fn_2
+            fn_2 = fn_1
+            fn_1 = fn
+            # # Option 2: Python 1-liner
+            # fn_2, fn_1 = fn_1, fn_1 + fn_2
+        return fn_1
