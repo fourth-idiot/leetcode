2395a6687ed3380a6bd6a52f6ae5abe9181c514c
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-07-05 20:18:02 -0500

    Time: 44 ms (66.40%), Space: 13.9 MB (54.45%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -4,30 +4,30 @@ class Solution:
         # if((n == 0) or (n == 1)): return n
         # return self.fib(n - 1) + self.fib(n - 2)
         
-        # Approach 2 (Dynamic programming - Memoization)
-        dp = {0: 0, 1: 1}
-        def helper(n):
-            if(n in dp):
-                return dp[n]
-            # Check if (n - 1) and (n - 2) are already computed
-            # Option 1 (Using standard x in dict syntax)
-            if((n - 1) not in dp):
-                dp[n - 1] = helper(n - 1)
-            if((n - 2) not in dp):
-                dp[n - 2] = helper(n - 2)
-            # # Option 2 (Python 1-liner using get() method on python dictionaries)
-            # return dp.get(n - 1, helper(n - 1)) + dp.get(n - 2, helper(n - 2))
-            return dp[n - 1] + dp[n - 2]
-        return helper(n)
+        # # Approach 2 (Dynamic programming - Memoization)
+        # dp = {0: 0, 1: 1}
+        # def helper(n):
+        #     if(n in dp):
+        #         return dp[n]
+        #     # Check if (n - 1) and (n - 2) are already computed
+        #     # Option 1 (Using standard x in dict syntax)
+        #     if((n - 1) not in dp):
+        #         dp[n - 1] = helper(n - 1)
+        #     if((n - 2) not in dp):
+        #         dp[n - 2] = helper(n - 2)
+        #     # # Option 2 (Python 1-liner using get() method on python dictionaries)
+        #     # return dp.get(n - 1, helper(n - 1)) + dp.get(n - 2, helper(n - 2))
+        #     return dp[n - 1] + dp[n - 2]
+        # return helper(n)
     
-        # # Approach 3 (Dynamic programming - Tabulation)
-        # if((n == 0) or (n == 1)):
-        #     return n
-        # dp = [0 for _ in range(n + 1)]
-        # dp[1] = 1
-        # for i in range(2, n + 1):
-        #     dp[i] = dp[i - 1] + dp[i - 2]
-        # return dp[n]
+        # Approach 3 (Dynamic programming - Tabulation)
+        if((n == 0) or (n == 1)):
+            return n
+        dp = [0 for _ in range(n + 1)]
+        dp[1] = 1
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]
         
         # # Approach 4 (Dynamic programming - Tabulation with state reduction)
         # # From the approach 3, you can see that we need only previous 2 previous at
