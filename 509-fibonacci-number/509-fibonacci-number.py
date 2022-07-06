c5bb44a249a2ccd82044f27cbe8364b57cf34694
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-07-05 20:17:51 -0500

    Time: 35 ms (86.37%), Space: 13.9 MB (54.45%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,24 +1,24 @@
 class Solution:
     def fib(self, n: int) -> int:
-        # Approach 1 (Recursion)
-        if((n == 0) or (n == 1)): return n
-        return self.fib(n - 1) + self.fib(n - 2)
+        # # Approach 1 (Recursion)
+        # if((n == 0) or (n == 1)): return n
+        # return self.fib(n - 1) + self.fib(n - 2)
         
-        # # Approach 2 (Dynamic programming - Memoization)
-        # dp = {0: 0, 1: 1}
-        # def helper(n):
-        #     if(n in dp):
-        #         return dp[n]
-        #     # Check if (n - 1) and (n - 2) are already computed
-        #     # Option 1 (Using standard x in dict syntax)
-        #     if((n - 1) not in dp):
-        #         dp[n - 1] = helper(n - 1)
-        #     if((n - 2) not in dp):
-        #         dp[n - 2] = helper(n - 2)
-        #     # # Option 2 (Python 1-liner using get() method on python dictionaries)
-        #     # return dp.get(n - 1, helper(n - 1)) + dp.get(n - 2, helper(n - 2))
-        #     return dp[n - 1] + dp[n - 2]
-        # return helper(n)
+        # Approach 2 (Dynamic programming - Memoization)
+        dp = {0: 0, 1: 1}
+        def helper(n):
+            if(n in dp):
+                return dp[n]
+            # Check if (n - 1) and (n - 2) are already computed
+            # Option 1 (Using standard x in dict syntax)
+            if((n - 1) not in dp):
+                dp[n - 1] = helper(n - 1)
+            if((n - 2) not in dp):
+                dp[n - 2] = helper(n - 2)
+            # # Option 2 (Python 1-liner using get() method on python dictionaries)
+            # return dp.get(n - 1, helper(n - 1)) + dp.get(n - 2, helper(n - 2))
+            return dp[n - 1] + dp[n - 2]
+        return helper(n)
     
         # # Approach 3 (Dynamic programming - Tabulation)
         # if((n == 0) or (n == 1)):
