272ffe5613f57934356a44fc806e2cc222dcd052
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-07-05 20:10:34 -0500

    Time: 28 ms (96.87%), Space: 13.9 MB (54.45%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -6,17 +6,17 @@ class Solution:
         # else:
         #     return self.fib(n - 1) + self.fib(n - 2)
         
-        # Approach 2 (Dynamic programming - Memoization)
-        dp = {0: 0, 1: 1}
-        def helper(n):
-            if(n in dp):
-                return dp[n]
-            if((n - 1) not in dp):
-                dp[n - 1] = helper(n - 1)
-            if((n - 2) not in dp):
-                dp[n - 2] = helper(n - 2)
-            return dp[n - 1] + dp[n - 2]
-        return helper(n)
+        # # Approach 2 (Dynamic programming - Memoization)
+        # dp = {0: 0, 1: 1}
+        # def helper(n):
+        #     if(n in dp):
+        #         return dp[n]
+        #     if((n - 1) not in dp):
+        #         dp[n - 1] = helper(n - 1)
+        #     if((n - 2) not in dp):
+        #         dp[n - 2] = helper(n - 2)
+        #     return dp[n - 1] + dp[n - 2]
+        # return helper(n)
     
         # # Approach 3 (Dynamic programming - Tabulation)
         # if((n == 0) or (n == 1)):
@@ -26,3 +26,13 @@ class Solution:
         # for i in range(2, n + 1):
         #     dp[i] = dp[i - 1] + dp[i - 2]
         # return dp[n]
+        
+        # Approach 4 (Dynamic programming - Tabulation with state reduction)
+        if((n == 0) or (n == 1)):
+            return n
+        fn_2, fn_1 = 0, 1
+        for _ in range(2, n + 1):
+            fn = fn_1 + fn_2
+            fn_2 = fn_1
+            fn_1 = fn
+        return fn_1
