ec0e50c6984f9cc7b32ec1a1dde1f68127dcb8c4
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-07-09 20:20:55 -0500

    Time: 1500 ms (56.43%), Space: 36.5 MB (13.20%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.py
+++ 1696-jump-game-vi/1696-jump-game-vi.py
@@ -1,5 +1,10 @@
 class Solution:
     def maxResult(self, nums: List[int], k: int) -> int:
+        # For every subproblem, we can find the maximum score one can get
+        # to rach the last index of the array starting from the i'th index.
+        # At any index i, dp[i] becomes nums[i] + max(dp[i + 1], ..., dp[min(n - 1, i + k)].
+        # To reduce its time complexity, we can the maximum value by storing dp values in a heap,
+        # then we can pop out all the maximum values which around beyond k steps from i'th index.
         dp = nums.copy()
         maxHeap = [(-1 * nums[len(nums) - 1], len(nums) - 1)]
         heapify(maxHeap)
