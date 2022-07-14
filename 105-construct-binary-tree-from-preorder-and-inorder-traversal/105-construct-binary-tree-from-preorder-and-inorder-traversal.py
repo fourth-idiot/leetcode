0dd19026f4394912ec45b154de0f0d1c4bde3a9c
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-07-13 19:35:30 -0500

    Time: 173 ms (66.20%), Space: 53.4 MB (59.72%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -7,6 +7,13 @@
 
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        # Preorder Traversal: Root -> Left -> Right
+        # Inorder Traversal: Left -> Root -> Right
+        # Hence, at any step, first value in the preorder traversal represents
+        # root of the binary tree at that point.
+        # If that value is at the i'th index in inorder list,
+        # then all the values to its left are part of left subtree.
+        # Similarly, all the values to its right are part of the right subtree.
         if(len(inorder) == 0):
             return None
         val = preorder.pop(0)
@@ -14,6 +21,8 @@ class Solution:
         idx = inorder.index(val)
         leftInorder = inorder[:idx]
         rightInorder = inorder[(idx + 1):]
+        # Since we traverse left node before right node in preorder tranversal,
+        # node.left needs to be evaluated before node.right
         node.left = self.buildTree(preorder, leftInorder)
         node.right = self.buildTree(preorder, rightInorder)
         return node
