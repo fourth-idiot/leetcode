4766d17869bb46c7976f4bf55a2835e21ef473db
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-07-14 21:55:37 -0500

    Time: 153 ms (86.33%), Space: 16.5 MB (69.88%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -1,11 +1,22 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        # Almost similar to the famous number of islands problem on LeetCode.
+        # We'll loop over all the elements of grid.
+        # If the element value is 1, that element is a part of some island.
+        # We find the extent of that island by performing dfs starting from that node.ArithmeticError
+        # While doing that, we set the element values to 0 to indicate that we've already visited that element,
+        # and we return the area of island traversed till now.
+        
         def dfs(i, j):
+            # Base condition: If we reach the end of the grid, or if the element value is zero,
+            # then we've reached the end of the island in that directions. Hence, we return 0
             if((i < 0) or (i >= numRows) or
                (j < 0) or (j >= numCols) or
                (not grid[i][j])):
                 return 0
+            # Mark the element visited by changing its value to 0.
             grid[i][j] = 0
+            # Call DFS recursively on four sides.
             return 1 + dfs(i - 1, j) + dfs(i + 1, j) + dfs(i, j - 1) + dfs(i, j + 1)
         
         numRows, numCols = len(grid), len(grid[0])
