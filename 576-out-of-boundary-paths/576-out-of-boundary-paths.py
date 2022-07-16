cd0d4f13b653efd78561688074a6f9f755a27ce1
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-07-15 22:22:19 -0500

    Time: 126 ms (86.44%), Space: 20.2 MB (21.74%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.py
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.py
@@ -1,15 +1,20 @@
 class Solution:
     def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
         @cache
-        def dfs(i, j, prevNumMoves):
+        def dfs(i, j, remainingMoves):
+            # Similar to the number of islands problem, here we can start DFS from (startRow, startColumn).
+            # Now, we need to check for three conditions:
+            # 1. If we are out of moves and we are still inside the bounds, then no paths are possible. Hence we return 0.
+            # 2. Else if we are out of bounds, then this is one possible path. Hence we return 1.
+            # 3. Else, we need to take sum of all possible paths by moving in four directions with one less move.
             isOutOfBound = (i < 0) or (i >= m) or (j < 0) or (j >= n)
-            if((prevNumMoves == maxMove) and (not isOutOfBound)):
+            if((remainingMoves == 0) and (not isOutOfBound)):
                 return 0
             elif(isOutOfBound):
                 return 1
             else:
-                return (dfs(i - 1, j, prevNumMoves + 1) + \
-                       dfs(i + 1, j, prevNumMoves + 1) + \
-                       dfs(i, j - 1, prevNumMoves + 1) + \
-                       dfs(i, j + 1, prevNumMoves + 1)) % (7 + (10 ** 9))
-        return dfs(startRow, startColumn, 0)
+                return (dfs(i - 1, j, remainingMoves - 1) + \
+                       dfs(i + 1, j, remainingMoves - 1) + \
+                       dfs(i, j - 1, remainingMoves - 1) + \
+                       dfs(i, j + 1, remainingMoves - 1)) % (7 + (10 ** 9))
+        return dfs(startRow, startColumn, maxMove)
